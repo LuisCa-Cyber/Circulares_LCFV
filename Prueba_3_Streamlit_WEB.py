af265f270bce5474e8aa42a100f0d923e9c25355
Luis Fernandez <luiscafer728@hotmail.com>
2025-01-30 13:42:37 +0000

    Update Prueba_3_Streamlit_WEB.py

--- Prueba_3_Streamlit_WEB.py
+++ Prueba_3_Streamlit_WEB.py
@@ -129,12 +129,6 @@ def get_base_messages():
 
 def run_chatbot():
 
-# Ocultar el icono de GitHub en Streamlit
-    st.markdown("""
-        <style>
-            .stApp header {visibility: hidden;}
-        </style>
-    """, unsafe_allow_html=True)
     # Mostrar el logo y el título en una sección fija
     logo_path = "Imagen2.png"
 
